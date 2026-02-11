b9905bf1290ecceec0097796ed5fad77db3db8ed
alvinchristian <alvch6@gmail.com>
2026-02-11 21:20:17 +0700

    (chore): change var to final

--- src/main/java/com/example/individualprep/service/VectorUtility.java
+++ src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -14,7 +14,7 @@ public class VectorUtility {
         return new double[] { 0.0, 0.0, 0.0 };
     }
 
-    public double[] multiply(double[] v1, int x) {
+    public double[] multiply(final double[] v1, final int x) {
         double[] result = new double[v1.length];
         for (int i = 0; i < v1.length; i++) {
             result[i] = v1[i] * x;
