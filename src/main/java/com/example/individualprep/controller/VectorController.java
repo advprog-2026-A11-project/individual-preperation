92adf6266e5a2ec60af3a1396f425306e68e45dc
alvinchristian <alvch6@gmail.com>
2026-02-11 21:46:09 +0700

    (chore): reformat whole codebase

--- src/main/java/com/example/individualprep/controller/VectorController.java
+++ src/main/java/com/example/individualprep/controller/VectorController.java
@@ -11,44 +11,44 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/vector")
 public class VectorController {
 
-    private final VectorUtility vectorUtility;
+  private final VectorUtility vectorUtility;
 
-    @Autowired
-    public VectorController(VectorUtility vectorUtility) {
-        this.vectorUtility = vectorUtility;
-    }
+  @Autowired
+  public VectorController(VectorUtility vectorUtility) {
+    this.vectorUtility = vectorUtility;
+  }
 
-    @PostMapping("/add")
-    public ResponseEntity<VectorResponse> add(@RequestBody VectorRequest request) {
-        double[] result = vectorUtility.add(request.vector1(), request.vector2());
-        return ResponseEntity.ok(new VectorResponse(result));
-    }
+  @PostMapping("/add")
+  public ResponseEntity<VectorResponse> add(@RequestBody VectorRequest request) {
+    double[] result = vectorUtility.add(request.vector1(), request.vector2());
+    return ResponseEntity.ok(new VectorResponse(result));
+  }
 
-    @PostMapping("/subtract")
-    public ResponseEntity<VectorResponse> subtract(@RequestBody VectorRequest request) {
-        double[] result = vectorUtility.subtract(request.vector1(), request.vector2());
-        return ResponseEntity.ok(new VectorResponse(result));
-    }
+  @PostMapping("/subtract")
+  public ResponseEntity<VectorResponse> subtract(@RequestBody VectorRequest request) {
+    double[] result = vectorUtility.subtract(request.vector1(), request.vector2());
+    return ResponseEntity.ok(new VectorResponse(result));
+  }
 
-    @PostMapping("/multiply")
-    public ResponseEntity<VectorResponse> multiply(@RequestBody VectorRequest request) {
-        if (request.scalar() == null) {
-            return ResponseEntity.badRequest().build();
-        }
-        double[] result = vectorUtility.multiply(request.vector1(), request.scalar());
-        return ResponseEntity.ok(new VectorResponse(result));
+  @PostMapping("/multiply")
+  public ResponseEntity<VectorResponse> multiply(@RequestBody VectorRequest request) {
+    if (request.scalar() == null) {
+      return ResponseEntity.badRequest().build();
     }
+    double[] result = vectorUtility.multiply(request.vector1(), request.scalar());
+    return ResponseEntity.ok(new VectorResponse(result));
+  }
 
-    @PostMapping("/dotProduct")
-    public ResponseEntity<VectorResponse> dotProduct(@RequestBody VectorRequest request) {
-        double result = vectorUtility.dotProduct(request.vector1(), request.vector2());
-        return ResponseEntity.ok(new VectorResponse(result));
-    }
+  @PostMapping("/dotProduct")
+  public ResponseEntity<VectorResponse> dotProduct(@RequestBody VectorRequest request) {
+    double result = vectorUtility.dotProduct(request.vector1(), request.vector2());
+    return ResponseEntity.ok(new VectorResponse(result));
+  }
 
-    @PostMapping("/norm")
-    public ResponseEntity<VectorResponse> norm(@RequestBody VectorRequest request) {
-        double result = vectorUtility.norm(request.vector1());
-        return ResponseEntity.ok(new VectorResponse(result));
-    }
+  @PostMapping("/norm")
+  public ResponseEntity<VectorResponse> norm(@RequestBody VectorRequest request) {
+    double result = vectorUtility.norm(request.vector1());
+    return ResponseEntity.ok(new VectorResponse(result));
+  }
 }
 
--- src/main/java/com/example/individualprep/service/VectorUtility.java
+++ src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -4,29 +4,29 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class VectorUtility {
-    
-    public double[] add(double[] v1, double[] v2) {
-        // TODO: Implement me properly!
-        return new double[] { 0.0, 0.0, 0.0 };
-    }
 
-    public double[] subtract(double[] v1, double[] v2) {
-        // TODO: Implement me properly!
-        return new double[] { 0.0, 0.0, 0.0 };
-    }
+  public double[] add(double[] v1, double[] v2) {
+    // TODO: Implement me properly!
+    return new double[] {0.0, 0.0, 0.0};
+  }
 
-    public double[] multiply(double[] v1, int x) {
-        // TODO: Implement me properly!
-        return new double[] { 0.0, 0.0, 0.0 };
-    }
-    
-    public double dotProduct(double[] v1, double[] v2) {
-        // TODO: Implement me properly!
-        return 0.0;
-    }
-    
-    public double norm(double[] v1) {
-        // TODO: Implement me properly!
-        return 0.0;
-    }
+  public double[] subtract(double[] v1, double[] v2) {
+    // TODO: Implement me properly!
+    return new double[] {0.0, 0.0, 0.0};
+  }
+
+  public double[] multiply(double[] v1, int x) {
+    // TODO: Implement me properly!
+    return new double[] {0.0, 0.0, 0.0};
+  }
+
+  public double dotProduct(double[] v1, double[] v2) {
+    // TODO: Implement me properly!
+    return 0.0;
+  }
+
+  public double norm(double[] v1) {
+    // TODO: Implement me properly!
+    return 0.0;
+  }
 }
