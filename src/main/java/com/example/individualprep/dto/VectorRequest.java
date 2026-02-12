9286fec302730b86b36583559fc139e75a48129e
Daffa Rayhan Ananda <127756775+daffuwu@users.noreply.github.com>
2026-02-12 23:31:27 +0700

    Revert "(feat): add arithmetic add feature"

--- src/main/java/com/example/individualprep/dto/VectorRequest.java
+++ src/main/java/com/example/individualprep/dto/VectorRequest.java
@@ -1,9 +1,9 @@
 package com.example.individualprep.dto;
 
 public record VectorRequest(
-        double[] vector1,
-        double[] vector2,
-        Integer scalar // Optional, only used for scalar multiplication
+    double[] vector1,
+    double[] vector2,
+    Integer scalar // Optional, only used for scalar multiplication
 ) {
 }
 
--- src/main/java/com/example/individualprep/service/VectorUtility.java
+++ src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -7,24 +7,48 @@ public class VectorUtility {
 
   public double[] add(double[] v1, double[] v2) {
     // TODO: Implement me properly!
-    return new double[]{0.0, 0.0, 0.0};
+    return new double[] { 0.0, 0.0, 0.0 };
   }
 
   public double[] subtract(double[] v1, double[] v2) {
-    // TODO: Implement me properly!
-    return new double[]{0.0, 0.0, 0.0};
+    if (v1.length != v2.length) {
+      throw new IllegalArgumentException("Two vectors must have the same length!");
+    }
+
+    double[] result = new double[v1.length];
+    for (int i = 0; i < v1.length; i++) {
+      result[i] = v1[i] - v2[i];
+    }
+
+    return result;
   }
 
-  public double[] multiply(double[] v1, int x) {
-    // TODO: Implement me properly!
-    return new double[]{0.0, 0.0, 0.0};
+  public double[] multiply(final double[] v1, final int x) {
+    double[] result = new double[v1.length];
+    for (int i = 0; i < v1.length; i++) {
+      result[i] = v1[i] * x;
+    }
+    return result;
   }
 
+  // Dot product of two vectors
   public double dotProduct(double[] v1, double[] v2) {
     // TODO: Implement me properly!
-    return 0.0;
+
+    // check if two vectors have the same length
+    if (v1.length != v2.length) {
+      throw new IllegalArgumentException("Two vectors must have the same length!");
+    }
+
+    double result = 0.0;
+    for (int i = 0; i < v1.length; i++) {
+      result += v1[i] * v2[i];
+    }
+
+    return result;
   }
 
+  // Norm of a vector
   public double norm(double[] v1) {
     // TODO: Implement me properly!
     return 0.0;
