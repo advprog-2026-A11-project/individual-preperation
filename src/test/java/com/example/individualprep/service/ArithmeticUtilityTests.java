83cecde0cb6606699e539e301311810d8149acbe
alvinchristian <alvch6@gmail.com>
2026-02-12 15:55:22 +0700

    fix linting

--- src/test/java/com/example/individualprep/service/ArithmeticUtilityTests.java
+++ src/test/java/com/example/individualprep/service/ArithmeticUtilityTests.java
@@ -1,9 +1,9 @@
 package com.example.individualprep.service;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ArithmeticUtilityTests {
 
