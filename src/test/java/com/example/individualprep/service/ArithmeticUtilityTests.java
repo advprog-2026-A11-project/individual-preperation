48353bf83858c7db5576a10cc202edbbeaa0d538
daffuwu <dananda41661@gmail.com>
2026-02-12 22:31:04 +0700

    Add unit tests to add feature and delete unnecessary unit tests in the branch

--- src/test/java/com/example/individualprep/service/ArithmeticUtilityTests.java
+++ src/test/java/com/example/individualprep/service/ArithmeticUtilityTests.java
@@ -15,91 +15,37 @@ class ArithmeticUtilityTests {
   }
 
   @Test
-  void multiply_twoPositiveNumbers_returnsProduct() {
-    assertEquals(6.0, util.multiply(2.0, 3.0), 1e-9);
+  void add_twoPositiveNumbers() {
+    assertEquals(15.5, util.add(10.0, 5.5), 1e-9);
   }
 
   @Test
-  void multiply_byZero_returnsZero() {
-    assertEquals(0.0, util.multiply(5.0, 0.0), 1e-9);
+  void add_positiveAndNegative() {
+    assertEquals(2.0, util.add(5.0, -3.0), 1e-9);
+    assertEquals(-2.0, util.add(-5.0, 3.0), 1e-9);
   }
 
   @Test
-  void multiply_negativeAndPositive_returnsNegativeProduct() {
-    assertEquals(-15.0, util.multiply(-3.0, 5.0), 1e-9);
+  void add_twoNegativeNumbers() {
+    assertEquals(-10.0, util.add(-4.0, -6.0), 1e-9);
   }
 
   @Test
-  void multiply_isCommutative() {
-    double a = 2.5;
-    double b = -4.0;
-    assertEquals(util.multiply(a, b), util.multiply(b, a), 1e-9);
+  void add_withZero() {
+    assertEquals(7.0, util.add(7.0, 0.0), 1e-9);
+    assertEquals(-3.0, util.add(0.0, -3.0), 1e-9);
   }
 
   @Test
-  void subtract_positiveNumber_withZero() {
-    double result = util.subtract(9.0, 0.0);
-    assertEquals(9.0, result);
+  void add_isCommutative() {
+    double a = 123.45;
+    double b = 67.89;
+    assertEquals(util.add(a, b), util.add(b, a), 1e-9);
   }
 
   @Test
-  void subtract_negativeNumber_withZero() {
-    double result = util.subtract(-3.0, 0.0);
-    assertEquals(-3.0, result);
-  }
-
-  @Test
-  void subtract_zeroWithPositiveNumber() {
-    double result = util.subtract(0.0, 7.0);
-    assertEquals(-7.0, result);
-  }
-
-  @Test
-  void subtract_zeroWithNegativeNumber() {
-    double result = util.subtract(0.0, -5.0);
-    assertEquals(5.0, result);
-  }
-
-  @Test
-  void subtract_twoPositiveNumbers() {
-
-    // case 1: num1 is bigger than num2
-    double result = util.subtract(9.0, 7.0);
-    assertEquals(2.0, result);
-
-    // case 2: num2 is bigger than num1
-    double result2 = util.subtract(5.0, 11.0);
-    assertEquals(-6.0, result2);
-
-    // case 3: num1 == num2
-    double result3 = util.subtract(4.0, 4.0);
-    assertEquals(0.0, result3);
-  }
-
-  @Test
-  void subtract_twoNegativeNumbers() {
-
-    // case 1: num1 is bigger than num2
-    double result = util.subtract(-2.0, -9.0);
-    assertEquals(7.0, result);
-
-    // case 2: num2 is bigger than num2
-    double result2 = util.subtract(-9.0, -2.0);
-    assertEquals(-7.0, result2);
-
-    // case 3: num1 == num2
-    double result3 = util.subtract(-8.0, -8.0);
-    assertEquals(0.0, result3);
-  }
-
-  @Test
-  void subtract_positiveNumber_withNegativeNumber() {
-    // case 1; num1 = positive, num2 = negative
-    double result = util.subtract(4.0, -2.0);
-    assertEquals(6.0, result);
-
-    // case 2: num1 = negative, num2 = positive
-    double result2 = util.subtract(-4.0, 5.0);
-    assertEquals(-9.0, result2);
+  void add_largeValues() {
+    // Testing boundary-like behavior with large doubles
+    assertEquals(2000000.0, util.add(1000000.0, 1000000.0), 1e-9);
   }
 }
